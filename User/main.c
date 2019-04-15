0f0baba83709b9356040549fb7c6e9bfa9e85621
jjw <591276288@qq.com>
2019-04-15 22:30:04 +0800

    first commit

---
+++ User/main.c
@@ -0,0 +1,57 @@
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdio.h>
+
+void RCC_Configuration_t(void)
+{
+  /* RCC system reset(for debug purpose) */
+  RCC_DeInit();
+  /* Enable HSE */
+  RCC_HSEConfig(RCC_HSE_ON);
+  /* Wait till HSE is ready */
+  ErrorStatus HSEStartUpStatus_t;
+  HSEStartUpStatus_t = RCC_WaitForHSEStartUp();
+  if (HSEStartUpStatus_t == SUCCESS)
+  {
+    /* Enable Prefetch Buffer */
+    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+    /* Flash 2 wait state */
+    FLASH_SetLatency(FLASH_Latency_2);
+    /* HCLK = SYSCLK */
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);
+    /* PCLK2 = HCLK/2 */
+    RCC_PCLK2Config(RCC_HCLK_Div1);
+    /* PCLK1 = HCLK/2 */
+    RCC_PCLK1Config(RCC_HCLK_Div2);
+    /* PLLCLK = 8MHz * 9 = 72 MHz */
+    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+    /* Enable PLL */
+    RCC_PLLCmd(ENABLE);
+    /* Wait till PLL is ready */
+    while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+    {}
+    /* Select PLL as system clock source */
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+    /* Wait till PLL is used as system clock source */
+    while (RCC_GetSYSCLKSource() != 0x08)
+    {}
+  }
+
+  /* Enable GPIO and devices clocks */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+                         RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD |
+                         RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF |
+                         RCC_APB2Periph_GPIOG,ENABLE);
+  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
+}
+
+int main(void)
+{
+  RCC_Configuration_t();
+ 
+  while(1)
+  {
+  }
+}
+
